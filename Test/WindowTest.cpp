5284a9d5498d80078f1f5f3d5d4aaf02e7bcd614
Chenqi <1002652154@qq.com>
2026-04-18 05:58:09 +0200

    update WindowTest

--- Test/WindowTest.cpp
+++ Test/WindowTest.cpp
@@ -12,27 +12,33 @@ class MainWindow : public Window {
 public:
 	using Window::Window;
 private:
-	Rect region = Rect(100, 100, 800, 500);
+	Size size = Size(800, 500);
 private:
+	virtual std::pair<Size, Size> CalculateMinMaxSize(Size size_ref) {
+		return { Size(100, 100), size_ref };
+	}
 	virtual Rect OnWindowSizeRefUpdate(Size size_ref) override {
-		region.size = UpdateChildSizeRef(child, region.size);
+		Rect region;
+		region.size = UpdateChildSizeRef(child, size);
+		region.point.x = (size_ref.width - region.size.width) / 2;
+		region.point.y = (size_ref.height - region.size.height) / 2;
 		return region;
 	}
 };
 
 
-class EmptyView : public CustomizedCursor<SolidColorBackground<Placeholder<Fixed, Fixed>, Color::Gray>, Cursor::Hand> {
-public:
-	EmptyView() {}
+class EmptyView : public CustomizedCursor<SolidColorBackground<Placeholder<Fixed, Fixed>, Color(Color::Yellow, 0x7F)>, Cursor::Hand> {
 private:
 	virtual void OnMouseEvent(MouseEvent event) override {
 		switch (event.type) {
-		case MouseEvent::RightDown:
+		case MouseEvent::LeftDown:
 			SetFocus();
-			background = background == Color::Gray ? Color::White : Color::Gray;
+			AeroSnapDraggingEffect(*this);
+			break;
+		case MouseEvent::WheelVertical:
+			background.alpha = std::max(0x00, std::min(0xFF, background.alpha + event.wheel_delta / 4));
 			Redraw(region_infinite);
 			break;
-		case MouseEvent::LeftDown: AeroSnapDraggingEffect(*this); break;
 		}
 	}
 	virtual void OnKeyEvent(KeyEvent event) override {
@@ -50,7 +56,14 @@ int main() {
 	desktop.AddWindow(new MainWindow(L"WindowTest", new ViewFrame(new EmptyView())));
 	desktop.EventLoop();
 
+	desktop.AddWindow(new MainWindow(L"WindowTest", new ViewFrameMutable(new EmptyView())));
+	desktop.EventLoop();
+
 	std::unique_ptr<EmptyView> view(new EmptyView());
+
 	desktop.AddWindow(new MainWindow(L"WindowTest", new ViewFrameRef(*view)));
+	desktop.EventLoop();
+
+	desktop.AddWindow(new MainWindow(L"WindowTest", new ViewFrameRefMutable(*view)));
 	desktop.EventLoop();
 }
