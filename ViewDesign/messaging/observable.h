3b0d15bd55403473326b86c8539b0d84cc4e8525
Chenqi <1002652154@qq.com>
2026-05-28 06:37:19 +0200

    observable eager erasure

--- ViewDesign/messaging/observable.h
+++ ViewDesign/messaging/observable.h
@@ -5,6 +5,7 @@
 
 #include <list>
 #include <functional>
+#include <stdexcept>
 
 
 namespace ViewDesign {
@@ -18,7 +19,7 @@ protected:
 		friend class Observable;
 	public:
 		Observer(const Observable& observable, std::function<void(Arg...)> callback) : observable(&observable), index(observable.AddObserver(*this)), callback(std::move(callback)) {}
-		~Observer() { if (observable) { observable->RemoveObserver(index); } }
+		~Observer() { if (observable != nullptr) { observable->RemoveObserver(index); } }
 	private:
 		ref_ptr<const Observable> observable;
 		std::list<ref_ptr<Observer>>::iterator index;
@@ -26,26 +27,50 @@ protected:
 	};
 
 protected:
-	~Observable() { for (auto observer : observer_list) { if (observer != nullptr) { observer->observable = nullptr; } } }
+	~Observable() {
+		if (current != observer_list.end()) {
+			std::terminate();
+		}
+		for (auto observer : observer_list) {
+			observer->observable = nullptr;
+		}
+	}
 
 private:
 	mutable std::list<ref_ptr<Observer>> observer_list;
+	mutable std::list<ref_ptr<Observer>>::iterator current = observer_list.end();
 private:
 	std::list<ref_ptr<Observer>>::iterator AddObserver(Observer& observer) const {
 		return observer_list.emplace(observer_list.end(), &observer);
 	}
 	void RemoveObserver(std::list<ref_ptr<Observer>>::iterator index) const {
-		*index = nullptr;
+		if (index == current) {
+			*index = nullptr;
+		} else {
+			observer_list.erase(index);
+		}
 	}
 
 protected:
 	void Notify(Arg... arg) const {
-		for (auto it = observer_list.begin(); it != observer_list.end();) {
-			if (*it == nullptr) {
-				it = observer_list.erase(it);
-			} else {
-				(*it++)->callback(arg...);
+		if (current != observer_list.end()) {
+			throw std::logic_error("Observable: Notify() called within a Notify()");
+		}
+		try {
+			for (current = observer_list.begin(); current != observer_list.end();) {
+				(*current)->callback(arg...);
+				if (*current == nullptr) {
+					current = observer_list.erase(current);
+				} else {
+					current++;
+				}
 			}
+		} catch (...) {
+			if (*current == nullptr) {
+				observer_list.erase(current);
+			}
+			current = observer_list.end();
+			throw;
 		}
 	}
 };
